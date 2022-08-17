8a45a70fcabed0f4fdeaa883708406e22836f128
muskbuster <sudeepkamat79@gmail.com>
2022-08-17 14:01:09 +0000

    trying out escrow

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,75 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::near_bindgen;
+use near_sdk::collections::LookupMap;
+use near_sdk::{env, log, near_bindgen, AccountId, Balance, Promise};
 
 #[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
-pub struct Contract {
-    // SETUP CONTRACT STATE
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct Escrow {
+    deposits: LookupMap<AccountId, Balance>,
+}
+
+impl Default for Escrow {
+    fn default() -> Self {
+        env::panic_str("Escrow should be initialized before usage")
+    }
 }
 
 #[near_bindgen]
-impl Contract {
-    // ADD CONTRACT METHODS HERE
-}
+impl Escrow {
+    #[init]
+    pub fn new() -> Self {
+        if env::state_exists() {
+            env::panic_str("ERR_ALREADY_INITIALIZED");
+        }
 
-/*
- * the rest of this file sets up unit tests
- * to run these, the command will be:
- * cargo test --package rust-template -- --nocapture
- * Note: 'rust-template' comes from Cargo.toml's 'name' key
- */
-
-// use the attribute below for unit tests
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use near_sdk::test_utils::{get_logs, VMContextBuilder};
-    use near_sdk::{testing_env, AccountId};
-
-    // part of writing unit tests is setting up a mock context
-    // provide a `predecessor` here, it'll modify the default context
-    fn get_context(predecessor: AccountId) -> VMContextBuilder {
-        let mut builder = VMContextBuilder::new();
-        builder.predecessor_account_id(predecessor);
-        builder
+        Self {
+            deposits: LookupMap::new(b"r".to_vec()),
+        }
     }
 
-    // TESTS HERE
+    pub fn deposits_of(&self, payee: &AccountId) -> Balance {
+        return match self.deposits.get(payee) {
+            Some(deposit) => deposit,
+            None => 0,
+        };
+    }
+
+    #[payable]
+    pub fn deposit(&mut self) {
+        if env::current_account_id() == env::signer_account_id() {
+            env::panic_str("ERR_OWNER_SHOULD_NOT_DEPOSIT");
+        }
+
+        let amount = env::attached_deposit();
+        let payee = env::signer_account_id();
+        let current_balance = self.deposits_of(&payee);
+        let new_balance = &(&current_balance + &amount);
+
+        self.deposits.insert(&payee, new_balance);
+
+        log!(
+            "{} deposited {} NEAR tokens. New balance {}",
+            &payee,
+            amount,
+            new_balance
+        );
+        // @TODO emit deposit event
+    }
+
+    #[payable]
+    pub fn withdraw(&mut self) {
+        let payee = env::signer_account_id();
+        let payment = self.deposits_of(&payee);
+
+        Promise::new(payee.clone()).transfer(payment);
+        self.deposits.insert(&payee, &0);
+
+        log!(
+            "{} withdrawn {} NEAR tokens. New balance {}",
+            &payee,
+            payment,
+            self.deposits_of(&payee)
+        );
+        // @TODO emit withdraw event
+    }
 }
